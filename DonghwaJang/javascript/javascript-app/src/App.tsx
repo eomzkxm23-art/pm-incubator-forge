5d9f629d6576cf948190059a258d259ca2275f57
dongsu2039-tech <dongsu2039@gmail.com>
2025-12-31 14:14:20 +0900

    feat: [장동화] for 루프 문제 풀이 [PMIF-x]

--- DonghwaJang/javascript/javascript-app/src/App.tsx
+++ DonghwaJang/javascript/javascript-app/src/App.tsx
@@ -3,11 +3,12 @@ import { ControlFlowIf } from './control_flow/ControlFlowIf'
 import { ControlFlowFor } from './control_flow/ControlFlowIFor'
 import { ControlFlowForSumExample } from './control_flow/ControlFlowIForsumExample'
 import { ControlFlowForSummation } from './control_flow/ControlFlowIForsummation'
-import { ControlFlowSwitch } from './control_flow/ControlFlowIswitch'
+import { ControlFlowSwitch } from './control_flow/ControlFlowISwitch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataType'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
 import { CalculationExample } from './math_operation/CalculationExample'
 import { FirstProblem } from './problem/FirstProblem'
+import { SecondProblem } from './problem/SecondProblem'
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
         <ControlFlowFor/>
         <ControlFlowForSummation/>
         <ControlFlowForSumExample/>
+        <SecondProblem/>
 
       </div>
     </>
